f9a2c76ca6b42e204b4f7dbe049068ed3ba13473
Mike Munene <mike.munene@moringaschool.com>
2021-05-24 23:26:37 +0300

    Modify the quotes.component.html

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   title = 'Quotes App';
-  quotes: Quote [] = [
+  quote: Quote [] = [
     new Quote (1,'Ashley','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
     new Quote (2,'Tony', 'Inspirational','Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', new Date(2019,7,12),0,0),
     new Quote (3,'Mike','Inspirational','That which does not kill us makes us stronger.','Friedrich Nietzsche', new Date(2019,7,12),0,0),
@@ -16,7 +16,19 @@ export class QuotesComponent implements OnInit {
     new Quote (5,'Jacob','Inspirational','We must not allow other people’s limited perceptions to define us.','Virginia Satir', new Date(2019,7,12),0,0),
     new Quote (6,'Samuel','Inspirational','Do what you can, with what you have, where you are.','Theodore Roosevelt', new Date(2019,7,12),0,0),
     new Quote (7,'Mellisa','Inspirational','This above all: to thine own self be true.','William Shakespeare', new Date(2019,7,12),0,0),
-  ] 
+  ];
+
+  get sortQuotes() {
+    return this.quote.sort((a, b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }  
+  addedQuote(quote: any){
+    let arraysize = this.quote.length;
+    quote.id = arraysize+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quote.push(quote)
+  }
 
 
   constructor() { }
