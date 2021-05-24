011c843dbbd7de55fe8acb15b513db0ca7e6545d
Mike Munene <mike.munene@moringaschool.com>
2021-05-24 18:25:32 +0300

    Modify the quotes-form component and make an array in quote.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  title = 'Quotes App';
+  quotes: Quote [] = [
+    new Quote (1,'Ashley','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
+    new Quote (2,'Tony', 'Inspirational','Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', new Date(2019,7,12),0,0),
+    new Quote (3,'Mike','Inspirational','That which does not kill us makes us stronger.','Friedrich Nietzsche', new Date(2019,7,12),0,0),
+    new Quote (4,'Brian','Inspirational','Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.','Bernard M. Baruch', new Date(2019,7,12),0,0),
+    new Quote (5,'Jacob','Inspirational','We must not allow other people’s limited perceptions to define us.','Virginia Satir', new Date(2019,7,12),0,0),
+    new Quote (6,'Samuel','Inspirational','Do what you can, with what you have, where you are.','Theodore Roosevelt', new Date(2019,7,12),0,0),
+    new Quote (7,'Mellisa','Inspirational','This above all: to thine own self be true.','William Shakespeare', new Date(2019,7,12),0,0),
+  ] 
+
 
   constructor() { }
 
