c84116b4b77429813b1a7adecf18417875b63b25
Mike Munene <mike.munene@moringaschool.com>
2021-05-25 06:03:07 +0300

    Modify array in qoutes-components.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,13 +9,13 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'Quotes App';
   quote: Quote [] = [
-    new Quote (1,'Ashley','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
-    new Quote (2,'Tony', 'Inspirational','Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', new Date(2019,7,12),0,0),
-    new Quote (3,'Mike','Inspirational','That which does not kill us makes us stronger.','Friedrich Nietzsche', new Date(2019,7,12),0,0),
-    new Quote (4,'Brian','Inspirational','Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.','Bernard M. Baruch', new Date(2019,7,12),0,0),
-    new Quote (5,'Jacob','Inspirational','We must not allow other people’s limited perceptions to define us.','Virginia Satir', new Date(2019,7,12),0,0),
-    new Quote (6,'Samuel','Inspirational','Do what you can, with what you have, where you are.','Theodore Roosevelt', new Date(2019,7,12),0,0),
-    new Quote (7,'Mellisa','Inspirational','This above all: to thine own self be true.','William Shakespeare', new Date(2019,7,12),0,0),
+    new Quote (1,'Ashley','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,25,5),0,0),
+    new Quote (2,'Tony', 'Inspirational','Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', new Date(2020,19,2),0,0),
+    new Quote (3,'Mike','Inspirational','That which does not kill us makes us stronger.','Friedrich Nietzsche', new Date(2020,6,2),0,0),
+    new Quote (4,'Brian','Inspirational','Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.','Bernard M. Baruch', new Date(2019,22,8),0,0),
+    new Quote (5,'Jacob','Inspirational','We must not allow other people’s limited perceptions to define us.','Virginia Satir', new Date(2019,11,1),0,0),
+    new Quote (6,'Samuel','Inspirational','Do what you can, with what you have, where you are.','Theodore Roosevelt', new Date(2013,18,9),0,0),
+    new Quote (7,'Mellisa','Inspirational','This above all: to thine own self be true.','William Shakespeare', new Date(2016,28,6),0,0),
   ];
 
   get sortQuotes() {
